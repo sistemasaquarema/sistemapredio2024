68040702eee96da0ef45803909e516d42cabf9ce
priscila.f <priscila.f@futebolcard.com.br>
2024-01-28 19:20:47 -0300

    fix

--- informativos_edicao.php
+++ informativos_edicao.php
@@ -68,6 +68,8 @@
       </div>
       <div class="col-sm-12">
         <label for="imagem" class="form-label">Imagem</label>
+      </div>
+      <div class="col-sm-12">
         <input type="file" class="form-control1" id="imagem" name="imagem" placeholder="" value="" autofocus required>
       </div>
       </br>
