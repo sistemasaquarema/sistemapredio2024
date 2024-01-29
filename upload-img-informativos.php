30f7d13a3623487332cc0474ed71c6f3f83d1455
priscila.f <priscila.f@futebolcard.com.br>
2024-01-28 21:09:57 -0300

    teste

--- upload-img-informativos.php
+++ upload-img-informativos.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $targetDir = 'Informativos/Imagens'; // Substitua pelo caminho real da pasta
+  $targetDir = 'informativo/imagens'; // Substitua pelo caminho real da pasta
   $targetFile = $targetDir . basename($_FILES['imagem']['name']);
   $uploadOk = true;
   $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
