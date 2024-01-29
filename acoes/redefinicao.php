feca555ce4e3725d252ba3b764a6c4485e7844cd
sistemasaquarema <sistemasaquaremapg@gmail.com>
2024-01-29 00:26:18 -0300

    teste

---
+++ acoes/redefinicao.php
@@ -0,0 +1,39 @@
+<?php
+
+    // iniciar uma nova sessão
+    session_start();
+
+    //chamar nossa conexao
+    require_once 'conexao.php';
+
+    if(isset($_POST['bt_cadastrar'])) {
+        //pegar os dados postados e fazer o escape-
+        $email= mysqli_real_escape_string($con, $_POST['email']);
+        $senha= md5(mysqli_real_escape_string($con, $_POST['senha']));
+
+        //Instrução SQL
+        $sql= "UPDATE  usuarios SET senha = '$senha' WHERE email = '$email'";
+        $assunto = "Você redefiniu a senha no Sistema Saquarema";
+        $mensagem = "Sua senha foi redefinida em Sistema Saquerema com sucesso";
+        $header = "From: saquarema";
+
+        mail($email, $assunto, $mensagem, $header);
+
+        //Executar instrucao SQL e verificar sucesso
+        if(mysqli_query($con, $sql)) {
+            $_SESSION['mensagem'] = "Redefinido com sucesso!!";
+            $_SESSION['status'] = "success";
+            header('Location: ../index.php');
+
+        }
+        else {
+            $_SESSION['mensagem'] = "falhou! Favor contatar o Administrador";
+            $_SESSION['status'] = "danger";
+            header('Location: ../index.php');
+        }
+
+        //Fechar conexao
+        mysqli_close($con);
+    }
+
+
